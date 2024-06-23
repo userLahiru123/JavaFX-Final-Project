cab5497fdad27c9ed5e07c9e6e1c40820a7d9505
userLahiru123 <thiwanka631@gmail.com>
2024-06-23 21:04:59 +0530

    first commit

--- ClothifyStore/src/main/java/Main.java
+++ ClothifyStore/src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/dashboard-for-employee-page-form.fxml")))));
+        stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/login-page-form.fxml")))));
         stage.show();
     }
 }
